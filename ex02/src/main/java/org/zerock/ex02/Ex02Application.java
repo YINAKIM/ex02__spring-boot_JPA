b1afdca34ebcc9833a136c846e5a4ea8d5122670
YINAKIM <yinaofficeg@gmail.com>
2021-09-20 17:08:48 +0900

    ex02 / set for use JPA

---
+++ ex02/src/main/java/org/zerock/ex02/Ex02Application.java
@@ -0,0 +1,11 @@
+package org.zerock.ex02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex02Application {
+
+    public static void main(String[] args) { SpringApplication.run(Ex02Application.class, args); }
+
+}
---
+++ ex02/src/test/java/org/zerock/ex02/Ex02ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.ex02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("ex02..............load SUCCESS!");
+    }
+
+}
