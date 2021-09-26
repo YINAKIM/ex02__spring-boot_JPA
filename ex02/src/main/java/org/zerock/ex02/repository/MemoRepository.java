ceb9815a88d0242ccccdcbf6cd1b36cf6c4c5730
YINAKIM <yinaofficeg@gmail.com>
2021-09-26 12:23:47 +0900

    쿼리메서드 이용, 간단한 select, paging, delete(1건씩만) 처리하기
    [플러그인 설치함]
    JPA Buddy: Default persistence unit initialized
    쿼리메서드 문장 키워드 추천해서 완성하게 해줌 개꿀ㅋㅋ

--- ex02/src/main/java/org/zerock/ex02/repository/MemoRepository.java
+++ ex02/src/main/java/org/zerock/ex02/repository/MemoRepository.java
@@ -1,10 +1,16 @@
 package org.zerock.ex02.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.ex02.entity.Memo;
+
+import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo,Long> {
 // 이렇게 인터페이스 선언만 해도 스프링빈으로 등록된다(스프링이 해당인터페이스타입에 맞는 객체를 생성해서 빈으로 등록한다.)
 
-
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);  //  쿼리메서드로 Mno범위에 맞는 엔티티list를 select
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageableInfo);
+    void deleteMemoByMnoLessThan(Long lassThanNum);
 }
--- ex02/src/test/java/org/zerock/ex02/repository/MemoRepositoryTests.java
+++ ex02/src/test/java/org/zerock/ex02/repository/MemoRepositoryTests.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.zerock.ex02.entity.Memo;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -252,4 +254,235 @@ public class MemoRepositoryTests {
                         memo0_.memo_text asc limit ?        //--------------> memoText asc (sort2조건)
             */
     }
+
+    /**
+     * 쿼리메서드로 엔티티객체 List select
+     * 쿼리키워드: findBy, Between, OrderBy, Desc
+     */
+    @Test
+    public void testQueryMethods(){
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L,80L);
+
+        for(Memo memo:list){
+            System.out.println(memo);
+        }
+        /*
+            Hibernate:
+                select
+                    memo0_.mno as mno1_0_,
+                    memo0_.memo_text as memo_tex2_0_
+                from
+                    tbl_memo memo0_
+                where
+                    memo0_.mno between ? and ?
+                order by
+                    memo0_.mno desc  // ------> findBy로 시작하면서, BETWEEN과 ORDER BY를 사용한 메서드이름대로 쿼리가 실행됨
+            Memo(mno=80, memoText=sample...80) // 80번부터 Desc해서 읽어옴
+            Memo(mno=79, memoText=sample...79)
+            Memo(mno=78, memoText=sample...78)
+            Memo(mno=77, memoText=sample...77)
+            Memo(mno=76, memoText=sample...76)
+            Memo(mno=75, memoText=sample...75)
+            Memo(mno=74, memoText=sample...74)
+            Memo(mno=73, memoText=sample...73)
+            Memo(mno=72, memoText=sample...72)
+            Memo(mno=71, memoText=sample...71)
+            Memo(mno=70, memoText=sample...70) //---> List<Memo엔티티 객체>로 리턴,
+        */
+    }
+
+
+    /**
+     * 쿼리메서드로 페이징처리하기
+     * 쿼리키워드: findBy, Between
+     * [1] vs [2] 결과log보고 어떻게 가져오는지 이해할 것
+     */
+    @Test
+    public void testQueryMethodWithPageable(){
+
+        // [1] asc
+        Pageable pageableInfoAsc = PageRequest.of(0,8,Sort.by("mno").ascending());  //--> 0페이지부터, 1페이지에 10건씩, mno acs로 정렬해서 페이징하라는 PageableInfoAsc
+        Page<Memo> result = memoRepository.findByMnoBetween(10L,50L, pageableInfoAsc);  // --> pageableInfo에서 asc했으니까 from인 10번부터 1페이지(사이즈 8건짜리:10~17) 먼저 읽어옴
+
+        result.get().forEach(memo -> { System.out.println(memo); });
+        /********************************************************************************
+            Hibernate:
+                select
+                    memo0_.mno as mno1_0_,
+                    memo0_.memo_text as memo_tex2_0_
+                from
+                    tbl_memo memo0_
+                where
+                    memo0_.mno between ? and ?
+                order by
+                    memo0_.mno asc limit ?
+            Hibernate:
+                select
+                    count(memo0_.mno) as col_0_0_
+                from
+                    tbl_memo memo0_
+                where
+                    memo0_.mno between ? and ?
+            Memo(mno=10, memoText=sample...10)  asc했으니까 from인 10번부터 1페이지먼저 읽어옴
+            Memo(mno=11, memoText=sample...11)
+            Memo(mno=12, memoText=sample...12)
+            Memo(mno=13, memoText=sample...13)
+            Memo(mno=14, memoText=sample...14)
+            Memo(mno=15, memoText=sample...15)
+            Memo(mno=16, memoText=sample...16)
+            Memo(mno=17, memoText=sample...17) (size 8건짜리:10~17)
+        *******************************************************************************/
+
+
+
+
+        // [2] desc
+        Pageable pageableInfoDesc = PageRequest.of(0,10,Sort.by("mno").descending());  //--> 0페이지부터, 1페이지에 10건씩, mno decs로 정렬해서 페이징하라는 PageableInfoDesc
+        Page<Memo> resultDesc = memoRepository.findByMnoBetween(10L,50L, pageableInfoDesc);  // --> pageableInfo에서 desc했으니까 to인 50번부터 1페이지(사이즈10건짜리:50~41) 먼저 읽어옴
+
+        resultDesc.get().forEach(memo -> { System.out.println(memo); });
+        /*******************************************************************************
+            Hibernate:
+                select
+                    memo0_.mno as mno1_0_,
+                    memo0_.memo_text as memo_tex2_0_
+                from
+                    tbl_memo memo0_
+                where
+                    memo0_.mno between ? and ?
+                order by
+                    memo0_.mno desc limit ?
+            Hibernate:
+                select
+                    count(memo0_.mno) as col_0_0_
+                from
+                    tbl_memo memo0_
+                where
+                    memo0_.mno between ? and ?
+            Memo(mno=50, memoText=sample...50)  desc했으니까 to인 50번부터 1페이지 먼저 읽어옴
+            Memo(mno=49, memoText=sample...49)
+            Memo(mno=48, memoText=sample...48)
+            Memo(mno=47, memoText=sample...47)
+            Memo(mno=46, memoText=sample...46)
+            Memo(mno=45, memoText=sample...45)
+            Memo(mno=44, memoText=sample...44)
+            Memo(mno=43, memoText=sample...43)
+            Memo(mno=42, memoText=sample...42)
+            Memo(mno=41, memoText=sample...41)  (size10건 짜리니까 :50~41)
+        *******************************************************************************/
+    }
+
+
+    /**
+     * 쿼리메서드로 삭제하기
+     * 키워드: deleteBy
+     * 어노테이션: @Transactional, @Commit(테스트코드는 이거 안쓰면 rollback이 default라서)
+     */
+    @Transactional
+    @Commit
+    @Test
+    public void testDeleteQueryMethod(){
+        memoRepository.deleteMemoByMnoLessThan(5L); // 5번미만은 삭제
+
+        /**** 정상실행log *******************************
+         Hibernate:
+         select
+         memo0_.mno as mno1_0_,
+         memo0_.memo_text as memo_tex2_0_
+         from
+         tbl_memo memo0_
+         where
+         memo0_.mno<?
+         ----------------------> 일단 구간select 해옴
+         Hibernate:
+         delete
+         from
+         tbl_memo
+         where
+         mno=?
+         .
+         .
+         . 이렇게 5번의 delete쿼리가 실행된다 ( 1건씩 delete )
+         .
+         .
+         Hibernate:
+         delete
+         from
+         tbl_memo
+         where
+         mno=?
+
+         deleteBy 쿼리메서드로 delete하면 이렇게 일단 구간을 select해온 후 "1건씩 delete" 하기때문에 일괄삭제가 안된다.
+        ***********************************************/
+
+
+        /**** 그리고, 잘못된 상황 log 참고 ******************************************************************************
+        [1] @Transactional 사용안하면? ERROR 로그 확인하기
+        Hibernate:
+        select
+        memo0_.mno as mno1_0_,
+        memo0_.memo_text as memo_tex2_0_
+        from
+        tbl_memo memo0_
+        where
+        memo0_.mno<?
+
+         org.springframework.dao.InvalidDataAccessApiUsageException: No EntityManager with actual transaction available for current thread - cannot reliably process 'remove' call;...
+         Caused by: javax.persistence.TransactionRequiredException: No EntityManager with actual transaction available for current thread - cannot reliably process 'remove' call
+         .
+         .
+         1 test completed, 1 failed
+         > Task :test FAILED
+
+
+        [2] @Commit안하고 test코드 돌리면? 5건만 테스트 후 DB조회 해보기
+         2021-09-26 11:53:29.591  INFO 3589 --- [    Test worker] o.s.t.c.transaction.TransactionContext   : Began transaction (1) for test context
+         [DefaultTestContext@4d3d9b6d
+            testClass = MemoRepositoryTests, testInstance = org.zerock.ex02.repository.MemoRepositoryTests@57b61cd7, testMethod = testDeleteQueryMethod@MemoRepositoryTests, testException = [null],
+            mergedContextConfiguration =
+            [
+                 WebMergedContextConfiguration@70b60f5c testClass = MemoRepositoryTests, locations = '{}', classes = '{class org.zerock.ex02.Ex02Application}', contextInitializerClasses = '[]',
+                 activeProfiles = '{}', propertySourceLocations = '{}', propertySourceProperties = '{org.springframework.boot.test.context.SpringBootTestContextBootstrapper=true}',
+                 contextCustomizers = set[
+                                         org.springframework.boot.test.autoconfigure.actuate.metrics.MetricsExportContextCustomizerFactory$DisableMetricExportContextCustomizer@23be09f9,
+                                         .
+                                         .
+                                         .
+                                         .
+                                        ],
+                 attributes = map[
+                                 'org.springframework.test.context.web.ServletTestExecutionListener.activateListener' -> true,
+                                 .
+                                 .
+                                 .
+                                 'org.springframework.test.context.event.ApplicationEventsTestExecutionListener.recordApplicationEvents' -> false
+                                 ]
+            ];
+         transaction manager [org.springframework.orm.jpa.JpaTransactionManager@1dba10c0];
+         rollback [true]
+         -------------------------------------------------(rollbak된거 확인!!)
+
+         Hibernate:
+         select
+         memo0_.mno as mno1_0_,
+         memo0_.memo_text as memo_tex2_0_
+         from
+         tbl_memo memo0_
+         where
+         memo0_.mno<?
+         2021-09-26 11:53:29.823  INFO 3589 --- [    Test worker] o.s.t.c.transaction.TransactionContext   : Rolled back transaction for test: .....-------------(rollbak된거 확인!!)
+
+
+         2021-09-26 11:53:29.840  INFO 3589 --- [ionShutdownHook] j.LocalContainerEntityManagerFactoryBean : Closing JPA EntityManagerFactory for persistence unit 'default'
+         2021-09-26 11:53:29.843  INFO 3589 --- [ionShutdownHook] com.zaxxer.hikari.HikariDataSource       : HikariPool-1 - Shutdown initiated...
+         2021-09-26 11:53:29.848  INFO 3589 --- [ionShutdownHook] com.zaxxer.hikari.HikariDataSource       : HikariPool-1 - Shutdown completed.
+         BUILD SUCCESSFUL in 7s
+         4 actionable tasks: 2 executed, 2 up-to-date
+         11:53:29 오전: Task execution finished ':test --tests "org.zerock.ex02.repository.MemoRepositoryTests.testDeleteQueryMethod"'.
+
+         --->  application에서는 에러없이실행된거같지만 DB가서 조회해보면 삭제 안되고 rollback되어있음
+        **********************************************************************************/
+    }
+
+
 }
