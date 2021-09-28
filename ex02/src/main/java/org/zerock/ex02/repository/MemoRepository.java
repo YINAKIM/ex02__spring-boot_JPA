e803c430edf406bb7bd3d9c2527002a914f20ef1
YINAKIM <yinaofficeg@gmail.com>
2021-09-28 23:54:16 +0900

    @Query 어노테이션으로 JPQL사용하기 note

--- ex02/src/main/java/org/zerock/ex02/repository/MemoRepository.java
+++ ex02/src/main/java/org/zerock/ex02/repository/MemoRepository.java
@@ -3,14 +3,30 @@ package org.zerock.ex02.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.ex02.entity.Memo;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo,Long> {
 // 이렇게 인터페이스 선언만 해도 스프링빈으로 등록된다(스프링이 해당인터페이스타입에 맞는 객체를 생성해서 빈으로 등록한다.)
 
+
+    // 쿼리메서드 이용
     List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);  //  쿼리메서드로 Mno범위에 맞는 엔티티list를 select
     Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageableInfo);
     void deleteMemoByMnoLessThan(Long lassThanNum);
+
+    // JPQL 이용
+    @Query(value = "select m from Memo m order by m.mno desc")
+    List<Memo> getListDesc();
+
+    // 파라미터 바인딩
+    @Transactional
+    @Modifying
+    @Query(value = "update Memo m set m.memoText = :memoText where m.mno = :mno")
+    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
 }
--- ex02/src/main/resources/note/note_useQuery.md
+++ ex02/src/main/resources/note/note_useQuery.md
@@ -22,3 +22,35 @@ JPA에서 쿼리 이용하기  쿼리메서드 / @Query / Querydsl
 
 
 * delete처리시 1건씩 select > delete 하므로 @Transactional사용(안쓰면 cannot reliably process 'remove' call), 근데 쿼리메서드는 일괄삭제 안된서 잘 안씀
+
+* 쿼리메서드는 메서드명이 쿼리라서 쿼리키워드가 사용된다. 따라서 join등의 복잡한 쿼리 사용은 불편하다. 그래서 쿼리메서드는 간단한 처리에만 사용하고, @Query를 사용하는 경우가 더 많음
+
+
+
+
+## @Query 어노테이션 사용
+* 필요한 데이터만 선별적으로 추출 가능 
+* DB에 맞는 순수 SQL사용 가능
+* insert, update, delete등의 DML(select제외) 처리 - @Modifying과 함께 사용
+* 메서드이름과 상관X, 추가한 어노테이션으로 원하는 처리 
+* *JPQL*로 value 작성  
+```
+    @Query(value = "select m from Memo m order by m.mno desc") 
+    List<Memo> getListDesc();
+```
+##### JPQL : 객체지향 쿼리(Java Persistence Query Language) 
+1. 테이블 대신에 Entity클래스를 이용
+2. 컬럼 대신 Entity클래스에 선언된 필드를 이용해서 작성
+3. SQL에서 사용되는 함수들도 동일하게 사용 가능 : AVG(), COUNT(), GROUP BY, ORDER BY 등
+
+##### @Query의 파라미터 바인딩 : where구문에 들어가는 파라미터 처리하기
+* '?1, ?2' :  1부터 시작하는 파라미터 순서를 이용하는 방식 
+* ':xxx' :  ':파라미터이름'을 활용하는 방식
+* '#{}' :  자바빈 스타일 이용
+
+  ```
+  // 파라미터 바인딩
+  @Transactional
+  @Modifying
+  @Query(value = "update Memo m set m.memoText = :memoText where m.mno = :mno")
+  int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);```
